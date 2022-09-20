f41c83ee46afafe74af1e1f42c2b356aa99ebbc6
Abram <israelvictory87@gmail.com>
2022-09-20 19:22:18 +0100

    Added simplejwt views

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,6 +7,12 @@ from accounts.views import (
     UpdateUserInformationAPIView
 )
 
+# SimpleJWT Imports
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 
 app_name = "accounts"
 
@@ -14,4 +20,7 @@ app_name = "accounts"
 urlpatterns = [
     path("register/", RegisterUserAPIView.as_view(), name="register_user"),
     path("update/<str:email>/", UpdateUserInformationAPIView.as_view(), name="get_update_user"),
+
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
