c229766e07d4ff31cbf9d2dacce70b5f42fa8806
Abram <israelvictory87@gmail.com>
2022-09-20 23:23:07 +0100

    Created ObjectTracker abstract model

---
+++ products/helpers.py
@@ -0,0 +1,19 @@
+# Django Imports
+from django.db import models
+
+# Native Imports
+import uuid
+
+
+class ObjectTracker(models.Model):
+    
+    # ids
+    id = models.BigAutoField(primary_key=True, unique=True)
+    uuid = models.UUIDField(default=uuid.uuid4, help_text="Object unique ID.")
+    
+    # datetime fields
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        abstract = True
