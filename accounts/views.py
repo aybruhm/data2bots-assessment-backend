d8dbba76a14946cba86081570f3cd96138901319
Abram <israelvictory87@gmail.com>
2022-09-20 18:24:34 +0100

    Created register user api view

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,52 @@
-from django.shortcuts import render
+# Rest Framework Imports
+from rest_framework import status, views, permissions
+from rest_framework.request import Request
+from rest_framework.response import Response
 
-# Create your views here.
+# YASG Imports
+from drf_yasg.utils import swagger_auto_schema
+
+# Payload Imports
+from rest_api_payload import success_response, error_response
+
+# Own Imports
+from accounts.serializers import (
+    RegisterUserSerializer,
+    UserSerializer
+)
+
+
+
+class RegisterUserAPIView(views.APIView):
+    serializer_class = RegisterUserSerializer
+    permission_classes = (permissions.AllowAny, )
+    
+    @swagger_auto_schema(request_body=serializer_class)
+    def post(self, request:Request) -> Response:
+        """
+        This function takes in a request object, 
+        validates the data in the request object, saves the
+        data if it's valid, and returns a response object
+        
+        :param request: This is the request object that is sent to the view
+        :type request: Request
+        :return: A response object.
+        """
+        
+        serializer = self.serializer_class(data=request.data)
+        
+        if serializer.is_valid():
+            serializer.save()
+            
+            payload = success_response(
+                status=True, message="User created successfully.",
+                data=serializer.data
+            )
+            return Response(data=payload, status=status.HTTP_201_CREATED)
+        
+        else:
+            payload = error_response(
+                status=False,
+                message=serializer.errors
+            )
+            return Response(data=payload, status=status.HTTP_400_BAD_REQUEST)
