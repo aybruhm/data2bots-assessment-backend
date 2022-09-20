8f1bd42355949dec5825ad7a2e7c90b3ce411c24
Abram <israelvictory87@gmail.com>
2022-09-20 19:06:42 +0100

    Created update user information api view

--- accounts/views.py
+++ accounts/views.py
@@ -14,6 +14,7 @@ from accounts.serializers import (
     RegisterUserSerializer,
     UserSerializer
 )
+from accounts.models import User
 
 
 
@@ -43,6 +44,78 @@ class RegisterUserAPIView(views.APIView):
                 data=serializer.data
             )
             return Response(data=payload, status=status.HTTP_201_CREATED)
+        
+        else:
+            payload = error_response(
+                status=False,
+                message=serializer.errors
+            )
+            return Response(data=payload, status=status.HTTP_400_BAD_REQUEST)
+        
+
+class UpdateUserInformationAPIView(views.APIView):
+    serializer_class = UserSerializer
+    permission_classes = (permissions.IsAuthenticated, )
+    
+    def get_user(self, email:str):
+        """
+        It returns a user object if the user exists, 
+        otherwise it raises an exception
+        
+        :param email: The email of the user you want to get
+        :type email: str
+        :return: The user object is being returned.
+        """
+        try:
+            user = User.objects.filter(email=email).first()
+            return user
+        except (User.DoesNotExist):
+            raise Exception("User does not exist!")
+            
+    
+    def get(self, request:Request, email:str) -> Response:
+        """
+        This function gets a user by email and 
+        returns a serialized version of the user
+        
+        :param request: The request object
+        :type request: Request
+        :param email: The email of the user to be retrieved
+        :type email: str
+        :return: A Response object with the payload and status code.
+        """
+        user = self.get_user(email=email)
+        serializer = self.serializer_class(user)
+        
+        payload = success_response(
+            status=True,
+            message="User retrieved successfully.",
+            data=serializer.data
+        )
+        return Response(data=payload, status=status.HTTP_200_OK)
+    
+    def put(self, request:Request, email:str) -> Response:
+        """
+        It updates the user's information.
+        
+        :param request: The request object
+        :type request: Request
+        :param email: The email of the user to be updated
+        :type email: str
+        :return: A response object.
+        """
+        user = self.get_user(email=email)
+        serializer = self.serializer_class(instance=user, data=request.data)
+        
+        if serializer.is_valid():
+            serializer.save()
+            
+            payload = success_response(
+                status=True,
+                message="User update successfully.",
+                data=serializer.data
+            )
+            return Response(data=payload, status=status.HTTP_202_ACCEPTED)
         
         else:
             payload = error_response(
