2fe4786fa2547d98bfdd304657b786966cbb24dd
Abram <israelvictory87@gmail.com>
2022-09-20 18:15:25 +0100

    Created register user and user serializers

---
+++ accounts/serializers.py
@@ -0,0 +1,24 @@
+# Rest Framework Imports
+from rest_framework import serializers
+
+# Own Imports
+from accounts.models import User
+
+
+class RegisterUserSerializer(serializers.Serializer):
+    firstname = serializers.CharField(required=True)
+    lastname = serializers.CharField(required=True)
+    username = serializers.CharField(required=True)
+    email = serializers.CharField(required=True)
+    password = serializers.CharField(required=True)
+
+
+class UserSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = User
+        fields = (
+            'id', 'uuid', 'firstname', 'lastname', 
+            'username', 'email', 'is_active', 'is_staff', 
+            'is_superuser', 
+        )
