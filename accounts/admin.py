7c97251176c761f346c6668fb4cd993aeba84447
Abram <israelvictory87@gmail.com>
2022-09-20 18:10:58 +0100

    Created user admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,10 @@
+# Django Imports
 from django.contrib import admin
 
-# Register your models here.
+# Own Imports
+from accounts.models import User
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'uuid', 'firstname', 'lastname', 'username', 'email', 'is_active', 'is_staff', 'is_superuser', )
