a4c4b7b6739bd0a4bd43d0ad1ae99303850a9a2e
Abram <israelvictory87@gmail.com>
2022-09-20 20:08:46 +0100

    Wrote test cases to test bearer_token, get valid and invalid user; and update valid and invalid user

--- accounts/tests/test_views.py
+++ accounts/tests/test_views.py
@@ -10,9 +10,11 @@ from rest_framework import status
 
 # Third Party Imports
 from rest_api_payload import success_response
+from rest_framework_simplejwt.tokens import RefreshToken
 
 # Own Imports
 from accounts.models import User
+from accounts.serializers import UserSerializer
 
 
 # Initialize api client
@@ -23,18 +25,49 @@ class AccountsTestCase(APITestCase):
     """Test case to register user"""
     
     def setUp(self) -> None:
-        self.valid_payload = {
+        self.valid_register_payload = {
             "firstname": "Israel",
             "lastname": "Abraham",
             "username": "abram",
             "password": "somereally_strongpassword_2002",
             "email": "abram@test.com",
         }
-        self.invalid_payload = {
+        self.invalid_register_payload = {
             "firstname": "Israel",
             "lastname": "",
             "password": "somereally_strongpassword_2002",
         }
+        self.login_payload = {
+            "email": "abram@test.com",
+            "password": "somereally_strongpassword_2002",
+        }
+        self.valid_update_user_payload = {
+            "firstname": "Abraham",
+            "lastname": "Israel",
+            "username": "abram",
+            "email": "abram@test.com",
+        }
+        self.invalid_update_user_payload = {
+            "firstname": 3434,
+            "lastname": "",
+            "username": ""
+        }
+        self.user = User.objects.create(
+            firstname="abram",
+            lastname="israel",
+            email="israel@test.com",
+            username="israel",
+            password="somereally_strongpassword_2002"
+        )
+        
+    @property
+    def bearer_token(self):
+        """
+        Get access token for user
+        """
+        user = User.objects.get(email="israel@test.com")
+        refresh = RefreshToken.for_user(user)
+        return {"HTTP_AUTHORIZATION": f"Bearer {refresh.access_token}"}
 
     def test_valid_register_user(self):
         """
@@ -42,8 +75,8 @@ class AccountsTestCase(APITestCase):
         """
         
         url = reverse("accounts:register_user")
-        response = client.post(url, data=self.valid_payload, format="json")
-        expected_data = success_response(status=True, message='User created successfully.', data=self.valid_payload)
+        response = client.post(url, data=self.valid_register_payload, format="json")
+        expected_data = success_response(status=True, message='User created successfully.', data=self.valid_register_payload)
         
         self.assertEqual(response.data, expected_data)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
@@ -54,6 +87,36 @@ class AccountsTestCase(APITestCase):
         """
         
         url = reverse("accounts:register_user")
-        response = client.post(url, data=self.invalid_payload, format="json")
+        response = client.post(url, data=self.invalid_register_payload, format="json")
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        
+    def test_get_valid_user(self):
+        """
+        Ensure we can get a single user
+        """
+        
+        url = reverse("accounts:get_update_user")
+        response = client.get(path=url, **self.bearer_token)     
+        serializer = UserSerializer(self.user)
+        serializer_data = success_response(status=True, message="User retrieved successfully.", data=serializer.data)
+
+        self.assertEqual(response.data, serializer_data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        
+    def test_update_valid_user(self):
+        """
+        Ensure we can update a user information
+        """
+        
+        url = reverse("accounts:get_update_user")
+        response = client.put(path=url, data=self.valid_update_user_payload, **self.bearer_token)     
+        self.assertEqual(response.status_code, status.HTTP_202_ACCEPTED)
+        
+    def test_update_invalid_user(self):
+        """
+        Ensure we can not update a user information with invalid data
+        """
+        url = reverse("accounts:get_update_user")
+        response = client.put(path=url, data=self.invalid_update_user_payload, **self.bearer_token)     
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         
