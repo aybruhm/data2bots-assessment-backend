3c332a5592f8f90321a2f907e4edb7d1ac09fd38
Abram <israelvictory87@gmail.com>
2022-09-20 20:51:03 +0100

    Created api doc schema and url path

--- core/urls.py
+++ core/urls.py
@@ -1,24 +1,35 @@
-"""core URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+# Django Imports
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+
+# Rest Framework Imports
+from rest_framework import permissions
+
+# DRF Yasg Imports``
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+# schema definition
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Data2bots Assessment API",
+      default_version='v1',
+      contact=openapi.Contact(email="israelvictory87@gmail.com"),
+      license=openapi.License(name="CC0-1.0 license"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     
     # authentication endpoints
     path("api/v1/auth/", include("accounts.urls")),
+    
+    # api docs
+    re_path(r'^generate_api_documentation(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    re_path(r'^docs/$', schema_view.with_ui('swagger', cache_timeout=0), name='api_docs'),
 ]
