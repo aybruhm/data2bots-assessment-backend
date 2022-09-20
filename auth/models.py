a6495f4de5d2fe19b1f71788c3fbf6fc10fb3a73
Abram <israelvictory87@gmail.com>
2022-09-20 17:57:48 +0100

    Created custom User model

--- auth/models.py
+++ auth/models.py
@@ -1,3 +1,39 @@
+# Django Imports
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.db import models
 
-# Create your models here.
+# Native Imports
+import uuid
+
+# Own Imports
+from auth.managers import UserManager
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    # unique ids
+    id = models.BigAutoField(auto_created=True, primary_key=True)
+    uuid = models.UUIDField(default=uuid.uuid4, help_text="User unique ID.")
+    
+    # information (necessaries)
+    firstname = models.CharField(max_length=255)
+    lastname = models.CharField(max_length=255)
+    username = models.CharField(max_length=255)
+    email = models.EmailField(db_index=True, unique=True)
+
+    # booleans
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["firstname", "lastname", "username"]
+
+    # set objects to UserManager
+    objects = UserManager()
+
+    def __str__(self) -> str:
+        return self.email
+
+    class Meta:
+        verbose_name_plural = "Users"
+        db_table = "users"
