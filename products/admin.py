73105fc8e88063fbc686415557cd590b4f54b000
Abram <israelvictory87@gmail.com>
2022-09-22 12:15:54 +0100

    Created product, order and payment admin

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,22 @@
+# Django Imports
 from django.contrib import admin
 
-# Register your models here.
+# Own Imports
+from products.models import (
+    Product, Order, Payment
+)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('title', 'price', 'quantity', )
+    
+    
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('user', 'status', )
+    
+
+@admin.register(Payment)
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = ('user', 'order', 'has_paid', 'status', )
